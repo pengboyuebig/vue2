377e3588775ec2c798da8f4ffd245cba9136eec6
pengboyue <2430633570@qq.com>
2022-04-06 13:42:41 +0800

    es的一些语法学习

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,11 +6,10 @@
     </div>
     <div class="main">
       <div>
-《斗破苍穹手游》龙游大陆，名传太古，全新职业太虚古龙将于10月29日版本上线。太虚古龙族乃斗气大陆魔兽界最强种族，拥有恐怖实力的斗者；化外之境，忘却修行之苦，静享人间之乐；全新玩法“苍穹纪事”斗兄斗帝，其乐无穷；全新时装“凤羽凰衣”上架，红光耀天，彩霞降临，宛若仙凰附身于上；福利多多，假日好礼、龙炎之灵新副本、珍宝斋满载福利等你开启全新斗气大陆。版本内容：1、【全新职业】-太虚古龙降世斗气大陆超然物外的魔兽王者家族，在唯一能与之抗衡的远古天凰族消失后，太虚古龙族成为了无可争议的魔兽界最强种族。战斗定位为一个中远程物理系输出核心，同时拥有近乎绝对的战场控制能力，攻防控全能陷阵战士。2、【全新时装】-凤羽凰衣远古天凰进阶后褪下的本命彩羽，由奇巧之能人于天险处偶然得之，系于彩纱之上辅以珍贵绸缎编织而成。出世时，红光耀天，彩霞降临，宛若仙凰附身于上。3、【魔兽系统】魔兽上阵惊天秘闻，魔兽将开启多个出战位，战斗中可轮替出战，实现更稳定的驻场效果。4、【新系统】-化外之境有一斗圣强者自号“云游圣人”，平生所好便是追寻新奇有趣的各种玩法，在纵情欢乐中以玩入道，自诩“趣圣”。趣圣将毕生所见所闻均记于其所创空间“化外之境”当中，欲让这天下斗者忘却修行之苦，静享人间之乐。5、【新玩法】苍穹纪事以“苍穹纪事”的卡牌游戏来纪念“炎帝”萧炎生平事迹。卡牌分为“天地玄黄”四种类型，卡牌之上皆为炎帝生平所识之人，所去之地，所见之物。
+          《斗破苍穹手游》龙游大陆，名传太古，全新职业太虚古龙将于10月29日版本上线。太虚古龙族乃斗气大陆魔兽界最强种族，拥有恐怖实力的斗者；化外之境，忘却修行之苦，静享人间之乐；全新玩法“苍穹纪事”斗兄斗帝，其乐无穷；全新时装“凤羽凰衣”上架，红光耀天，彩霞降临，宛若仙凰附身于上；福利多多，假日好礼、龙炎之灵新副本、珍宝斋满载福利等你开启全新斗气大陆。版本内容：1、【全新职业】-太虚古龙降世斗气大陆超然物外的魔兽王者家族，在唯一能与之抗衡的远古天凰族消失后，太虚古龙族成为了无可争议的魔兽界最强种族。战斗定位为一个中远程物理系输出核心，同时拥有近乎绝对的战场控制能力，攻防控全能陷阵战士。2、【全新时装】-凤羽凰衣远古天凰进阶后褪下的本命彩羽，由奇巧之能人于天险处偶然得之，系于彩纱之上辅以珍贵绸缎编织而成。出世时，红光耀天，彩霞降临，宛若仙凰附身于上。3、【魔兽系统】魔兽上阵惊天秘闻，魔兽将开启多个出战位，战斗中可轮替出战，实现更稳定的驻场效果。4、【新系统】-化外之境有一斗圣强者自号“云游圣人”，平生所好便是追寻新奇有趣的各种玩法，在纵情欢乐中以玩入道，自诩“趣圣”。趣圣将毕生所见所闻均记于其所创空间“化外之境”当中，欲让这天下斗者忘却修行之苦，静享人间之乐。5、【新玩法】苍穹纪事以“苍穹纪事”的卡牌游戏来纪念“炎帝”萧炎生平事迹。卡牌分为“天地玄黄”四种类型，卡牌之上皆为炎帝生平所识之人，所去之地，所见之物。
       </div>
     </div>
     <div class="tooter">
-
     </div>
   </div>
 </template>
@@ -19,27 +18,141 @@
 export default {
   name: 'Login',
   mounted () {
+    // var a
+    // function add () {
+    //   a = 12
+    // }
+    // add()
+
+    // let foo = 'foo'
+    // function bar () {
+    //   console.log('bar')
+    // }
+    // class Person {
+
+    // }
+    // // console.log(foo)
+    // bar()
+    // var p = new Person()
+
+    /* function foo (...args) {
+      console.log(args)
+    }
+    foo('Hellow Word', '1111') */
+    // function foo (...args) {
+    //   console.log(args)
+    // }
+    // const name = 'why'
+    // const age = 18
+    // foo`helow ${name} word ${age}`
+    /* function foo (arg) {
+      console.log(arg, arg.length, arg[0])
+    }
+    foo([]) */
+    // function foo ({name, age} = {name: 10, age: 20}) {
+    //   console.log(name, age)
+    // }
+    // function foo (a, b, c = 12, d) {
+    //   console.log(arguments.length)
+    //   console.log('name,age')
+    // }
+    // foo(1, 2, 4, 6)
+    /* let Foo = () => {
+      console.log('foo', this)
+    }
+    console.log(Foo.prototype, 'foo.prototype')
+    let f = new Foo()
+    console.log(f) */
+  /*   console.log(1111)
+    console.log(Object.keys({name: 'boyue', age: 12}))
+
+    // let dep = new Dep()
+    // console.log(dep)
+    let a = [1, 45, 7, 4, 85, 152, 611]
+    console.log(a.indexOf(4))
+    console.log(a.includes(4))
+    let b = 'sadadsadasdsadasdqwerwewfsdgdybvbc'
+    console.log(b.indexOf('d'))
+    console.log(b.includes('d'))
+
+    console.log(Math.pow(2, 2), 2 ** 2)
+ */
+    /* const obj = {
+      name: 'why',
+      age: 18,
+      height: 124
+    }
+    console.log(Object.entries(obj), 'Object.entries(obj)')
+    console.log(Object.entries('qewqe'), 'Object.entries(string)')
+    console.log(Object.entries([1, 5, 7, 1, 2, 4]), 'Object.entries(array)')
+    console.log(Object.entries(obj), Object.fromEntries(Object.entries(obj)))
+    let abc = [[1, 2], [3, 4], [5, 6], [7, 6]]
+    console.log(abc, Object.fromEntries(abc))
+    const bigInt = 9007199254740991n
+    console.log(bigInt + 1n, bigInt + 2n) */
+    // let a = 'asdwertgfd'
+    // console.log(a, a.padStart(15, 'e'))
+    // console.log(a, a.padEnd(20, 'g'))
+    // const foo = ''
+    // const result1 = foo || '默认值'
+    // const result2 = foo ?? '默认值'
+    // console.log(result1, result2)
+    // let obj1 = {
+    //   name: 'name',
+    //   age: 'age',
+    //   sex: 'sex',
+    //   danger: 'danger',
+    //   pro: 'pro'
+    // }
+    // console.log(Object.entries(obj1))
+    // console.log(Object.fromEntries(Object.entries(obj1)))
+    // let arr = ['name', 'age', 'danger', 'sex']
+    // console.log(Object.entries(arr))
+    // console.log(Object.seal)
+    // var array = ['ABC', true, 12, -5]
+    // console.log(array.valueOf()) // true
+    // function foo () {
+    //   console.log(this)
+    // }
+    // var obj1 = {
+    //   name: 'obj1',
+    //   foo: foo
+    // }
+    // var obj2 = {
+    //   name: 'obj2',
+    //   obj1: obj1.foo
+    // }
+    // obj2.obj1()
+
   }
+
 }
 </script>
 
 <style scoped>
 .main{
 height: 200px;
-overflow-y: scroll;
+overflow-y: hidden;
 word-wrap: normal;
 /* white-space: pre-wrap; */
 color: #ff9700;
 }
+.main div{
+  width: 788px;
+}
+.main:hover{
+overflow-y: scroll;
+overflow-x: hidden;
+}
 .main::-webkit-scrollbar-track{
-background: red;
+background: transparent;
 border-radius: 6px;
 }
 .main::-webkit-scrollbar{
 width: 6px;
 }
 .main::-webkit-scrollbar-thumb{
-background: yellowgreen;
+background: #757575;
 border-radius: 6px;
 }
 .top{
