a25425086e32037a6bf292e2b7b677275c2e8ff4
pengboyue <2430633570@qq.com>
2022-04-12 11:09:48 +0800

    pro—start

--- src/views/html/index.vue
+++ src/views/html/index.vue
@@ -1,15 +1,41 @@
 <template>
   <div>
+    <!-- <form action="" method="post">
+      <keygen></keygen>
 
+      <input type="text" value="1111">
+    </form> -->
+    <!-- <input list="browsers">
+    <datalist id="browsers">
+      <option value="Internet Explorer">
+      <option value="Firefox">
+      <option value="Chrome">
+      <option value="Opera">
+      <option value="Safari">
+    </datalist> -->
+    <label for="color">color</label>
+    <input type="color" id="color" ref='color' @change="getColor">
+    <br>
+    <input type="date">
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Html'
+  name: 'Html',
+  data () {
+    return {
+      color: '#bfa'
+    }
+  },
+  methods: {
+    getColor () {
+      console.log(this.$refs.color.value)
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
 
 </style>
